383781c797a8402c791a4ffb6d5f12a14a254689
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-27 15:04:26 +0530

    Create Album List and Song List

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Navbar from './Navbar'
+import AlbumItem from './AlbumItem'
+import { albumsData, songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
     <>
-     <Navbar/> 
+    <Navbar/>
+    <div className='mb-5'>
+      <h1 className='my-5 font-bold text-2xl text-white'>Featured Charts</h1>
+      <div className='flex overflow-auto'>
+      {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
+      
+    </div> 
+    <div className='mb-5'>
+      <h1 className='my-5 font-bold text-2xl text-white'>Today's biggest hits</h1>
+      <div className='flex overflow-auto'>
+      {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
+      
+    </div> 
     </>
   )
 }
 
 export default DisplayHome
+  
--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
           <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
           <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer text-white'>Install App</p>
-          <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center '>S</p>
+          <p className='bg-blue-500 text-black w-7 h-7 rounded-full flex items-center justify-center '>A</p>
         </div>
     </div> 
     <div className='flex items-center gap-2 mt-4'>
@@ -20,6 +20,7 @@ const Navbar = () => {
         <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
         <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer'>Poadcasts</p>
     </div>
+    
     </>
   )
 }
