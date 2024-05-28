dabb9fc5517fee6942ee648dae3fe08b19b1033c
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-28 08:55:38 +0530

    Create Music Album Page

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -10,14 +10,14 @@ const DisplayHome = () => {
     <Navbar/>
     <div className='mb-5'>
       <h1 className='my-5 font-bold text-2xl text-white'>Featured Charts</h1>
-      <div className='flex overflow-auto'>
+      <div className='flex overflow-auto text-white'>
       {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
       </div>
       
     </div> 
     <div className='mb-5'>
       <h1 className='my-5 font-bold text-2xl text-white'>Today's biggest hits</h1>
-      <div className='flex overflow-auto'>
+      <div className='flex overflow-auto text-white'>
       {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
       </div>
       
