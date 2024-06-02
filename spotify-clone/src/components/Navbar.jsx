8b321c79a942c3188b1b710b8370dad07f55cc34
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-06-02 23:06:57 +0530

    Navigation to Previous Page and Next Page

--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const navigate = useNavigate()
     <>
     <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
-            <img onClick={()=>navigate} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
-            <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
+            <img onClick={()=>navigate(-1)} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
+            <img onClick={()=>navigate(1)} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
         </div>
         <div className='flex items-center gap-4'>
           <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
